e1786c2c0e624c8951a67f56d020bb0f42ff4512
Yunus TAN <yunustan0623@gmail.com>
2024-02-12 13:37:29 +0300

    py

---
+++ Longest_Common.java
@@ -0,0 +1,9 @@
+public class Longest_Common {
+    public static void main(String [] args){
+        String arr[] = {"flower","flow","flight"};
+        String result = "";
+        for (String string : arr) {
+            System.out.println(string);
+        }
+    }
+}
